37bde6a57cbc4d14795692c9d19df61d32e758ff
Leomotors (Ubuntu) <59821765+Leomotors@users.noreply.github.com>
2022-03-05 22:50:21 +0700

    Added 2nd Model

--- food-busters-ai/index.ts
+++ food-busters-ai/index.ts
@@ -1,9 +1,15 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 
-import { checkVersion, getMLResult, makeRes, VersionRejectReason } from "./lib";
+import {
+    checkVersion,
+    getMLLayerModel,
+    getMLResult,
+    makeRes,
+    VersionRejectReason,
+} from "./lib";
 
 const MinAppVersion = 306;
-const MinWebVersion = 2;
+const MinWebVersion = 4;
 
 const index: AzureFunction = async (context: Context, req: HttpRequest) => {
     const { body, headers } = req;
@@ -24,6 +30,11 @@ const index: AzureFunction = async (context: Context, req: HttpRequest) => {
         return;
     }
 
+    if (body.modelType != "P" && body.modelType != "U") {
+        context.res = makeRes(400, `Invalid Model Type: ${body.modelType}`);
+        return;
+    }
+
     if (!body.image) {
         context.res = makeRes(400, "Missing Image");
         return;
@@ -36,7 +47,9 @@ const index: AzureFunction = async (context: Context, req: HttpRequest) => {
 
     let result;
     try {
-        result = await getMLResult(body.image);
+        if (body.modelType == "P") result = await getMLResult(body.image);
+
+        if (body.modelType == "U") result = await getMLLayerModel(body.image);
     } catch (err) {
         context.res = makeRes(500, `Error executing Model: ${err}`);
         return;
--- food-busters-ai/lib/index.ts
+++ food-busters-ai/lib/index.ts
@@ -26,14 +26,66 @@ const labelMap: { [key: number]: { name: string; nutrition: FoodNutrition } } =
 
 // const mroot = "food-busters-ai/model";
 let net: tf.GraphModel;
+let lnet: tf.LayersModel;
+
+export async function getMLLayerModel(img: string) {
+    if (!lnet)
+        lnet = await tf.loadLayersModel(
+            "file://food-busters-ai/model-U/model.json"
+        );
+
+    const decodedImage = tf.node.decodeImage(
+        Buffer.from(img.replace(/^data:image\/.+;base64,/, ""), "base64"),
+        3
+    );
+
+    const transformed = tf.image
+        .resizeBilinear(decodedImage, [224, 224])
+        // stupid mistake
+        .cast("float32")
+        .div(255)
+        .expandDims(0);
+
+    const res = lnet.predict(transformed) as tf.Tensor;
+
+    // @ts-ignore
+    const arr = (await res.array())[0] as number[];
+
+    console.log(
+        `Omelet: ${arr[0] * 100}%, Chicken: ${arr[1] * 100}%, None: ${
+            arr[2] * 100
+        }%`
+    );
+
+    // C++ Way lol
+    let max = arr[0];
+    let maxpos = 0;
+    for (let i = 1; i < arr.length - 1; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+            maxpos = i;
+        }
+    }
+
+    const result: MLResult = {
+        foodName: labelMap[maxpos + 1].name,
+        foodNutrition: labelMap[maxpos + 1].nutrition,
+        confidence: max,
+        version: Version,
+    };
+
+    tf.dispose(decodedImage);
+    tf.dispose(transformed);
+    tf.dispose(res);
+
+    return result;
+}
 
 export async function getMLResult(img: string) {
-    // const model = tf.io.fileSystem(`${mroot}/model.json`);
-    // const m1 = tf.io.fileSystem(`${mroot}/group1-shard1of3.bin`);
-    // const m2 = tf.io.fileSystem(`${mroot}/group1-shard2of3.bin`);
-    // const m3 = tf.io.fileSystem(`${mroot}/group1-shard3of3.bin`);
-
-    if (!net) net = await tf.loadGraphModel(process.env.ML_ENDPOINT as string);
+    if (!net)
+        net = await tf.loadGraphModel(
+            "file://food-busters-ai/model/model.json"
+        );
 
     const decodedImage = tf.node.decodeImage(
         Buffer.from(img.replace(/^data:image\/.+;base64,/, ""), "base64"),
@@ -53,7 +105,7 @@ export async function getMLResult(img: string) {
         foodName: labelMap[detected].name,
         foodNutrition: labelMap[detected].nutrition,
         // @ts-ignore
-        score: (await obj[2].array())[0][0],
+        confidence: (await obj[2].array())[0][0],
         version: Version,
     };
 
